3b960db36aa1a294735ee4e74ce4899dc50df849
yucheng0 <55428017+yucheng0@users.noreply.github.com>
2021-09-07 12:30:12 +0800

    Add files via upload
    first

---
+++ show.php
@@ -0,0 +1,28 @@
+<!Doc html>
+<header>
+<meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale = 1, maximum-scale=1">
+</header>
+
+<title>
+ </title>
+
+<style type="text/css"></style>
+<script type="text/javascript">
+
+
+</script>
+<body>
+
+
+<Form>     </Form>
+
+<?php
+echo $_GET['a'];
+
+echo "<br>";
+echo $_GET['b']; 
+?>
+
+</body>
+</html>
